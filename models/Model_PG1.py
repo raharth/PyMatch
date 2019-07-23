92c0620230687f55f25a596056d1ff447fd65ce8
raharth <goltz.jonas@googlemail.com>
2019-07-24 00:28:26 +0200

    Adding model files

---
+++ models/Model_PG1.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+
+    def __init__(self, n_classes):
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 6, 3)
+        self.bn2 = nn.BatchNorm2d(6)
+        self.fc1 = nn.Linear(6 * 28 * 28, 500)
+        self.bn3 = nn.BatchNorm1d(500)
+        self.fc2 = nn.Linear(500, n_classes)
+
+    def forward(self, X, train=True, device='cpu'):
+        if train:
+            self.train()
+        else:
+            self.eval()
+
+        x = X.to(device)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = x.reshape(X.shape[0], -1)
+        x = self.fc1(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        out = F.softmax(x, dim=1)
+        return out
