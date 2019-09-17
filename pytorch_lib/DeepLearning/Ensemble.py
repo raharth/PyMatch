e4c7fc0b9e6d0d4c4ca87f2103a0a1d0fc5323d6
Jonas Goltz <jonas.goltz@gi-de.com>
2019-09-17 15:18:10 +0200

    Ensemble:     - Doc-strings     - majority voting, overhauled and simplyfied the prediction and data loader prediction method to be consistent with the majority voting

--- pytorch_lib/DeepLearning/Ensemble.py
+++ pytorch_lib/DeepLearning/Ensemble.py
@@ -12,33 +12,83 @@ class Ensemble:
             self.learners.append(trainer_factory(**t_args))
         self.epochs_run = 0
 
-        self.losses = []
+        # self.losses = [] # @todo I don't think that this is actually used anywhere
 
-    def predict(self, x, device='cpu'):
-        # @todo pass args to the learner
-        y_preds = [leaner.predict(x, device, return_prob=True) for leaner in self.learners]
+    def predict(self, x, device='cpu', return_prob=True, learner_args=None):
+        """
+        Predicting a data tensor.
+
+        Args:
+            x (torch.tensor): data
+            device: device to run the model on
+            return_prob: return probability or class label
+            learner_args: additional learner arguments (this may not include the 'return_prob' argument)
+
+        Returns:
+            prediction with certainty measure.
+            return_prob = True:
+                (Mean percentage, standard deviation of probability)
+            return_prob = False:
+                (Majority vote, percentage of learners voted for that label)
+
+        """
+        if learner_args is None:
+            learner_args = {}
+
+        y_preds = [leaner.predict(x, device, return_prob=return_prob, **learner_args) for leaner in self.learners]
         y_preds = torch.stack(y_preds)
-        y_pred_mean = y_preds.mean(dim=0)
-        y_pred_std = y_preds.std(dim=0)
-        return y_pred_mean, y_pred_std
 
-    def predict_data_loader(self, data_loader, device='cpu', return_true=False):
-        y_pred_m = []
-        y_pred_s = []
+        if return_prob:
+            return y_preds.mean(dim=0), y_preds.std(dim=0)
+
+        return self.majority_vote(y_preds)
+
+    def predict_data_loader(self, data_loader, device='cpu', return_true=False, return_prob=False):
+        """
+        Predicting an entire torch data loader.
+
+        Args:
+            data_loader: data loader containing the data to predict
+            device: device to run the model on
+            return_true: return the true values of the data loader
+            return_prob: return probabilistic result (majority voting if not)
+
+        Returns:
+            prediction, certainty measure (, true values)
+
+        """
+        y_pred = []
         y_true = []
         for x, y in data_loader:
-            y_pred = self.predict(x, device=device)
-            y_pred_m += [y_pred[0]]
-            y_pred_s += [y_pred[1]]
+            y_pred += [self.predict(x, device=device, return_prob=return_prob)]
             y_true += [y]
-        y_pred_m = torch.cat(y_pred_m)
-        y_pred_s = torch.cat(y_pred_s)
+
+        y_pred = torch.tensor(y_pred)
+        y_pred_m = y_pred[:, 0]
+        y_pred_certainty = y_pred[:, 1]
+
         if return_true:
             y_true = torch.cat(y_true)
-            return y_pred_m, y_pred_s, y_true
-        return y_pred_m, y_pred_s
+            return y_pred_m, y_pred_certainty, y_true
+
+        return y_pred_m, y_pred_certainty
 
     def train(self, epochs, device, checkpoint_int=10, validation_int=10, restore_early_stopping=False, verbose=1):
+        """
+        Trains each learner of the ensemble for a number of epochs
+
+        Args:
+            epochs: number of epochs to train each learner
+            device: device to run the models on
+            checkpoint_int: every checkpoint_int iterations the model is checkpointed
+            validation_int:  every validation_int iterations the model is validated
+            restore_early_stopping: restores the best performing weights after training
+            verbose: verbosity
+
+        Returns:
+            None
+
+        """
         for trainer in self.learners:
             if verbose == 1:
                 print('Trainer {}'.format(trainer.name))
@@ -46,14 +96,45 @@ class Ensemble:
                           validation_int=validation_int, restore_early_stopping=restore_early_stopping)
 
     def dump_checkpoint(self, path=None, tag='checkpoint'):
+        """
+        Dumps a checkpoint for each learner.
+
+        Args:
+            path: source folder of the checkpoints
+            tag: addition tags of the checkpoints
+
+        Returns:
+            None
+        """
         for trainer in self.learners:
             trainer.dump_checkpoint(path=path, tag=tag)
 
     def load_checkpoint(self, path=None, tag='checkpoint'):
+        """
+        Loads a set of checkpoints, one for each learner
+
+        Args:
+            path: source folder of the checkpoints
+            tag: addition tags of the checkpoints
+
+        Returns:
+            None
+
+        """
         for trainer in self.learners:
             trainer.load_checkpoint(path=path, tag=tag)
 
     def run_validation(self, device='cpu'):
+        """
+        Validates the ensemble on the validation dataset.
+
+        Args:
+            device: device to run the evaluation on
+
+        Returns:
+            predicted and true labels for each learner
+
+        """
         y_pred_learners = []
         y_true_learners = []
 
@@ -66,6 +147,51 @@ class Ensemble:
             y_pred_learners += [torch.cat(y_pred)]
             y_true_learners += [torch.cat(y_true)]
         return y_pred_learners, y_true_learners
+
+    def train_upto(self, epochs, device='cpu', checkpoint_int=10, validation_int=10, restore_early_stopping=False, verbose=1):
+        """
+        The primarily purpose of this method is to return to training after an interrupted trainings cycle of the ensemble.
+
+        Args:
+            epochs: max number of epochs to run the learners for
+            device: device to run the models on
+            checkpoint_int: every checkpoint_int iterations the model is checkpointed
+            validation_int:  every validation_int iterations the model is validated
+            restore_early_stopping: restores the best performing weights after training
+            verbose: verbosity
+
+        Returns:
+            None
+
+        """
+        for learner in self.learners:
+            train_epochs = epochs - learner.epochs_run
+            if verbose == 1:
+                print('Trainer {} - train for {} epochs'.format(learner.name, train_epochs))
+            learner.train(epochs=train_epochs, device=device, checkpoint_int=checkpoint_int,
+                          validation_int=validation_int, restore_early_stopping=restore_early_stopping)
+
+    def majority_vote(self, y_vote):
+        """
+        Evaluates the predictions of the ensemble as a majority voting.
+
+        Args:
+            y_vote: probabilistic output of the ensemble as a torch tensor of the predictions, where dim=0 are the learners
+
+        Returns:
+            prediction and percentage of that prediction
+
+        """
+        y_pred = []
+        y_count = []
+
+        for y in y_vote.transpose(0, 1):
+            val, count = torch.unique(y, return_counts=True)
+            y_pred += [val[count.argmax()].item()]
+            y_count += [count.argmax() / len(self.learners)]
+        return torch.tensor(y_pred), torch.tensor(y_count)
+
+
 
 class BaysianEnsemble(Ensemble):
 
