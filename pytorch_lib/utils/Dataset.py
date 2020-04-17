fc9e00986b2576546f291939a18824a1a0ff05b0
Jonas Goltz <jonas.goltz@gi-de.com>
2020-04-17 12:03:06 +0200

    #9 added in memory dataloader and one that continuously loads from the drive

--- pytorch_lib/utils/Dataset.py
+++ pytorch_lib/utils/Dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torchvision import datasets
+from torch.utils import data
 
 class ImageFolderWithPaths(datasets.ImageFolder):
     """Custom dataset that includes image file paths. Extends
@@ -16,3 +17,57 @@ class ImageFolderWithPaths(datasets.ImageFolder):
             tuple_with_path = (original_tuple + (path,))
             return tuple_with_path
         return original_tuple
+
+
+class InMemoryDataset(data.Dataset):
+
+    def __init__(self, data, labels):
+        self.labels = labels
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        # Load data and get label
+        X = self.data[index]
+        y = self.labels[index]
+        return X, y
+
+
+class HardDriveDataset(data.Dataset):
+
+    def __init__(self, list_IDs, labels):
+        """
+        Dataset for large data. It loads data on demand.
+        Also see: "https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel"
+
+        Example:
+            Let ID be the Python string that identifies a given sample of the dataset.
+            It assumes:
+                partition = {'train': ['id-1', 'id-2', 'id-3'], 'validation': ['id-4']}
+                labels = {'id-1': 0, 'id-2': 1, 'id-3': 2, 'id-4': 1}
+
+            And can be used by:
+                training_set = Dataset(partition['train'], labels)
+                training_generator = data.DataLoader(training_set, **params)
+
+        Args:
+            list_IDs:   file names for each data point
+            labels:     labels according to the file names
+        """
+        self.labels = labels
+        self.list_IDs = list_IDs
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = torch.load('data/' + ID + '.pt')
+        y = self.labels[ID]
+
+        return X, y
