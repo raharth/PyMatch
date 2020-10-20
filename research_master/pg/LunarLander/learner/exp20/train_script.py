f575a65fa6630250cc8538e48dfddd18ca742201
Jonas <goltz.jonas@googlemail.com>
2020-10-20 11:00:51 +0200

    experiment files

---
+++ research_master/pg/LunarLander/learner/exp20/train_script.py
@@ -0,0 +1,41 @@
+import sys
+
+from pymatch.DeepLearning.hat import EnsembleHat
+import pymatch.ReinforcementLearning.callback as cb
+from pymatch.ReinforcementLearning.learner import GreedyValueSelection
+from pymatch.ReinforcementLearning.torch_gym import TorchGym
+from pymatch.utils.experiment import Experiment
+from pymatch.utils.functional import interactive_python_mode
+from pymatch.DeepLearning.ensemble import Ensemble
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if interactive_python_mode():
+    path_scipt = 'research_master/pg/policy_gradient.py'
+    root = 'research_master/pg/exp8'
+else:
+    path_scipt = sys.argv[0]
+    root = sys.argv[1]
+
+experiment = Experiment(root=root)
+factory = experiment.get_factory()
+params = experiment.get_params()
+params['factory_args']['learner_args']['dump_path'] = root
+Model = experiment.get_model_class()
+experiment.document_script(path_scipt, overwrite=params['overwrite'])
+experiment.start(overwrite=params['overwrite'])
+
+factory_args = params['factory_args']
+learner = factory(Model=Model,
+                  model_args=factory_args['model_args'],
+                  env_args=factory_args['env_args'],
+                  optim_args=factory_args['optim_args'],
+                  memory_args=factory_args['memory_args'],
+                  learner_args=factory_args['learner_args'],
+                  name='policy_gradient')
+
+# learner.load_checkpoint(f'{root}/checkpoint')
+learner.fit(**params['fit'])
+# learner.resume_training(params['n_epochs'], 'cpu', restore_early_stopping=False, verbose=False)
+experiment.finish()
