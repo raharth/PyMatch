a9c3953b53bcdac54a4b12dd77a108ccdf92dc40
Jonas <goltz.jonas@googlemail.com>
2020-11-17 19:16:42 +0100

    #68 experiment files

---
+++ research_master/DQN/CartPole/boosting/exp_58_8/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+
+    def __init__(self, core, out_nodes):
+        super(Model, self).__init__()
+        self.core = core
+        self.fc1 = nn.Linear(24, out_nodes)
+
+    def forward(self, x):
+        x = self.core(x)
+        x = self.fc1(x)
+        return x
+
+    def parameters_module(self):
+        core_params, head_params = [], []
+        for np in self.named_parameters():
+            if np[0].split('.')[0] == 'core':
+                core_params += [np[1]]
+            else:
+                head_params += [np[1]]
+        return core_params, head_params
