57cb249d6f69c7f976a5631ee4ae6af6465dede0
Jonas <goltz.jonas@googlemail.com>
2021-03-14 11:09:18 +0100

    Refactoring Selection Strategies, to have  a general pre and post pipeline that works for all policies

--- pymatch/ReinforcementLearning/selection_policy.py
+++ pymatch/ReinforcementLearning/selection_policy.py
@@ -8,8 +8,26 @@ from pymatch.DeepLearning.pipeline import Pipeline
 
 
 class SelectionPolicy:
-    # @todo really necessary?
     def __init__(self, pre_pipeline=[], post_pipeline=[]):
+        """
+        Abstraction of Selection policy.
+        This is primarily used to have the pre and post pipeline available to all Selection Policies
+
+        Args:
+            pre_pipeline:   List of functions/objects sequentially applied to the input before predicted by the agent.
+                            This can be of use when using e.g. MC Dropout, where the input has to be stacked multiple
+                            times before it is predicted.
+            post_pipeline:  List of functions/objects sequentially applied to the output of the agent.
+                            This is useful if used with ensembles, where the output of the networks have to be
+                            aggregated or if certain measures have to be estimated.
+
+        Info:
+            Why is the selection policy a wrapper around a model in the first place instead of the policy a part of the
+            agent?
+
+            Using this approach it is possible to have a greedy evaluation policy, while still having a non-greedy
+            policy for the training. Using this approach you can also simply alter the Selection Policy after training.
+        """
         self.pre_pipeline = pre_pipeline
         self.post_pipeline = post_pipeline
 
@@ -25,6 +43,11 @@ class SelectionPolicy:
         for pipe in self.post_pipeline:
             x = pipe(x)
         return x
+
+    def __call__(self, agent, observation):
+        observation = self.preparation(observation)
+        x = self.forward(agent, observation)
+        return self.aggregation(x)
 
 # class Softmax_Selection(SelectionPolicy):
 #
@@ -50,13 +73,12 @@ class SelectionPolicy:
 #         else:
 #             return q_values.argmax(dim=1)
 
-class PolicyGradientActionSelection:
+class PolicyGradientActionSelection(SelectionPolicy):
     """
     Probability based selection strategy, used for Policy Gradient
     """
-
-    def __call__(self, agent, observation):
-        agent.model.to(agent.device)
+    def forward(self, agent, observation):
+        agent.to(agent.device)
         probs = agent.model(observation.to(agent.device))
         dist = Categorical(probs.squeeze())
         action = dist.sample()
@@ -64,22 +86,14 @@ class PolicyGradientActionSelection:
         return action.item(), log_prob
 
 
-class BayesianDropoutPGActionSelection:
-    def __init__(self, predictions: int, reduce_hat=hat.EnsembleHatStd()):
-        """
-        Probability based selection strategy, used for Policy Gradient, using multiple drop out forward passes
-        to estimate the reliability of the prediction.
-
-        Args:
-            predictions:    number of iterations used for the bayesian ensemble
-            reduce_hat:     ensemble hat reducing the ouput of the ensemble to a single probability distribution
-        """
-        self.predictions = predictions
-        self.reduce_hat = reduce_hat
-
-    def __call__(self, agent, observation):
+class BayesianDropoutPGActionSelection(SelectionPolicy):
+    """
+    Probability based selection strategy, used for Policy Gradient, using multiple drop out forward passes
+    to estimate the reliability of the prediction.
+    """
+    def forward(self, agent, observation):
         observation = observation.to(agent.device)
-        agent.model.to(agent.device)
+        agent.to(agent.device)
         action_probs = agent.model(torch.cat(self.predictions * [observation]))
         prob_mean, prob_std = self.reduce_hat(action_probs)
         dist = Categorical(prob_mean.squeeze())
@@ -89,29 +103,27 @@ class BayesianDropoutPGActionSelection:
 
 
 class QActionSelection(SelectionPolicy):
-    def __init__(self, temperature=1., **kwargs):
+    def __init__(self, temperature=1., *args, **kwargs):
         """
         Temperature based exponential selection strategy
 
         Args:
-            temperature:
+            temperature:    Temperature, which controls the degree of randomness.
         """
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
         self.temperature = temperature
 
-    def __call__(self, agent, observation):
-        agent.model.to(agent.device)
-        observation = self.preparation(observation)
-        qs = agent.model(observation.to(agent.device))
-        qs = self.aggregation(qs)
-        probs = F.softmax(qs / self.temperature, dim=1)
+    def forward(self, agent, observation):
+        agent.to(agent.device)
+        qs = agent(observation.to(agent.device))
+        probs = F.softmax(qs / self.temperature, dim=1)     # @todo dim=1 might cause trouble with ensembles?
         dist = Categorical(probs.squeeze())
         action = dist.sample()
         return action.item()
 
 
 class EpsilonGreedyActionSelection(SelectionPolicy):
-    def __init__(self, action_space, epsilon=.9, **kwargs):
+    def __init__(self, action_space, epsilon=.1, **kwargs):
         """
         Epsilon greedy selection strategy, choosing the best or with p=1-epsilon choosing a random action
 
@@ -123,12 +135,10 @@ class EpsilonGreedyActionSelection(SelectionPolicy):
         self.action_space = action_space
         self.epsilon = epsilon
 
-    def __call__(self, agent, observation):
-        agent.model.to(agent.device)
-        observation = self.preparation(observation)
-        qs = agent.model(observation.to(agent.device))
-        qs = self.aggregation(qs)
-        if np.random.uniform() < self.epsilon:
+    def forward(self, agent, observation):
+        agent.to(agent.device)
+        qs = agent(observation.to(agent.device))
+        if np.random.uniform() > self.epsilon:
             return qs.argmax().item()
         return np.random.choice(self.action_space)
 
@@ -136,38 +146,28 @@ class EpsilonGreedyActionSelection(SelectionPolicy):
 class GreedyValueSelection(SelectionPolicy):
     """
     Choosing the best possible option, necessary for evaluation
-    @todo abstract pre and post-pipeline
     """
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def __call__(self, agent, observation):
-        observation = self.preparation(observation)
+    def forward(self, agent, observation):
         qs = agent(observation.to(agent.device))
-        qs = self.aggregation(qs)
         return qs.argmax().item()
 
 
 class NormalThompsonSampling(SelectionPolicy):
-    def __init__(self, **kwargs):
-        """
-        Implementation of Thompson sampling based in the Normal distribution.
-        Estimates the distribution over a model, sampling from it.
-        Args:
-            pre_pipes:  pipeline elements before the agent
-            post_pipes: pipeline elements after the agent the last element of this list has to provide the
-                        parameterization of the distribution
-        @ todo  a better abstraction would be using a general distribution, defined in the constructor. Then the last
-                pipe element has to provide the final inputs to that distribution, right now it can only use the Normal
-                distribution but no other.
-                Why is this part of the sampling in the first place?
-        """
-        # self.repeater = hat.InputRepeater(n_iter)
-        super().__init__(**kwargs)
-
-    def __call__(self, agent, observation):
+    """
+    Implementation of Thompson sampling based in the Normal distribution.
+    Estimates the distribution over a model, sampling from it.
+    Args:
+        pre_pipes:  pipeline elements before the agent
+        post_pipes: pipeline elements after the agent the last element of this list has to provide the
+                    parameterization of the distribution
+    @ todo  a better abstraction would be using a general distribution, defined in the constructor. Then the last
+            pipe element has to provide the final inputs to that distribution, right now it can only use the Normal
+            distribution but no other.
+            Why is this part of the sampling in the first place?
+    """
+    def forward(self, agent, observation):
         pipeline = Pipeline(pipes=self.pre_pipes + [agent] + self.post_pipes)
-        with torch.no_grad():
+        with torch.no_grad():   # @todo why am I using a no grad here? This policy can then not be used for training?
             with eval_mode(agent):
                 y_mean, y_std = pipeline(observation)
         shape = y_mean.shape
