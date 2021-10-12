83b7d1e9c3a189f373a676d5fd02e9ba5c65bf60
Jonas <goltz.jonas@googlemail.com>
2021-10-12 17:05:10 +0200

    adding test

---
+++ tests/test_experiment/simple_DQN/factory.py
@@ -0,0 +1,19 @@
+import torch
+import pymatch.ReinforcementLearning.learner as rl
+
+
+def factory(Model, model_args, optim_args, learner_args, crit_args, name, env):
+    model = Model(**model_args)
+
+    optim = torch.optim.SGD(model.parameters(), **optim_args)
+    crit = torch.nn.MSELoss(**crit_args)
+
+    l_args = dict(learner_args)
+    l_args['name'] = f"{learner_args['name']}_{name}"
+
+    return rl.QLearner(env=env,
+                       model=model,
+                       optimizer=optim,
+                       crit=crit,
+                       fitter=rl.DQNFitter(),
+                       **l_args)
---
+++ tests/test_experiment/simple_DQN/model.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+
+    def __init__(self, in_nodes, out_nodes):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(in_nodes, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 100)
+        self.fc4 = nn.Linear(100, out_nodes)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.fc4(x)
+        return x
