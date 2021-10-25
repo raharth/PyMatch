1fff9cc48b21a52639cebc3c8317acefcb0ad6ff
Jonas <goltz.jonas@googlemail.com>
2021-10-25 17:01:20 +0200

    Bump version: 1.7.1 → 1.7.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pymatch',
-      version='1.7.1',
+      version='1.7.2',
       description='PyTorch wrapper for Deep Learning',
       url='https://github.com/raharth/PyMatch',
       author='Jonas Goltz',
